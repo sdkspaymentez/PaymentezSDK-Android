607c8ae3e5b2e9fca9cbc57c2adc782e23fcf89e
Francisco Tamburri <ftamburri@fennoma.com.ar>
2020-10-27 14:16:56 -0300

    Word fennoma removed

--- PaymentezSDK/src/androidTest/java/com/paymentez/plazez/sdk/ExampleInstrumentedTest.java
+++ PaymentezSDK/src/androidTest/java/com/paymentez/plazez/sdk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ar.com.fennoma.paymentezsdk.test", appContext.getPackageName());
+        assertEquals("com.paymentez.plazez.sdk.test", appContext.getPackageName());
     }
 }
--- app/src/androidTest/java/com/paymentez/plazez/sdktester/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/paymentez/plazez/sdktester/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ar.com.fennoma.paymentezsdkholder", appContext.getPackageName());
+        assertEquals("com.paymentez.plazez.sdk", appContext.getPackageName());
     }
 }
